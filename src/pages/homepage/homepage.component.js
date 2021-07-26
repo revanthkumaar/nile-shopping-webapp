0a3eecf707ce0a02a2e156c4334523d95f42cf72
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-26 19:41:03 +0530

    first checkin

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,17 @@
+//imports that are needed
+import React from 'react';
+
+//functional component named "HomePage"
+const HomePage = () => {
+
+    return(
+        <div>
+
+            <p>this is home page component</p>
+
+        </div>
+    )
+
+}
+//this component must be made importable
+export default HomePage;
