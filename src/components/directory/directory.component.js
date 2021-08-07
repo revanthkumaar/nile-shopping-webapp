8102382c4729f1d254e8ef0d675aa1a4096b0791
revanthkumaar <iamrevanthkumar@gmail.com>
2021-08-07 18:41:41 +0530

    checkindasda

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,68 +1,60 @@
-import React from 'react'
+import React from "react";
 
 //maintain state in react component
 class Directory extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sections: [
+        {
+          title: "veggies",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          linkUrl: "veggies",
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: "",
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          linkUrl: "",
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "",
+        },
+      ],
+    };
+  }
 
-    constructor(){
-
-      super();
-       this.state = {
-         sections: [
-           {
-             title: "veggies",
-             imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-             id: 1,
-             linkUrl: "veggies",
-           },
-           {
-             title: "jackets",
-             imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-             id: 2,
-             linkUrl: "",
-           },
-           {
-             title: "sneakers",
-             imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-             id: 3,
-             linkUrl: "",
-           },
-           {
-             title: "womens",
-             imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-             size: "large",
-             id: 4,
-             linkUrl: "",
-           },
-           {
-             title: "mens",
-             imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-             size: "large",
-             id: 5,
-             linkUrl: "",
-           },
-         ]
-       };
-    }
-
-
-    
   render() {
-      
     return (
       <div className="directory-component">
-          {
-              this.state.sections.map(({id, ...otherSectionProps}) => {
-                  console.log(otherSectionProps);
-              })
-          }
+        {this.state.sections.map(({ id, ...otherSectionProps }) => {
+          return (
+            <div className="menu-item">
+              <p>{otherSectionProps.title}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
 export default Directory;
-
-
-
-
-
--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,16 +1,13 @@
 //imports that are needed
-import React from 'react';
-import Directory from '../../components/directory/directory.component'
+import React from "react";
+import Directory from "../../components/directory/directory.component";
 //functional component named "HomePage"
 const HomePage = () => {
-
-    return(
-        <div className="homepage-component">
-            <p>this is home page component</p>
-            <Directory/>
-        </div>
-    )
-
-}
+  return (
+    <div className="homepage-component">
+      <Directory />
+    </div>
+  );
+};
 //this component must be made importable
 export default HomePage;
