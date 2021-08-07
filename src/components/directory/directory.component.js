1003999a5a9d2396ec4023a0247295219dff9a88
revanthkumaar <iamrevanthkumar@gmail.com>
2021-08-07 18:24:42 +0530

    othersectionprops check

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -5,13 +5,43 @@ class Directory extends React.Component {
 
     constructor(){
 
-        super();
-        this.state = {
-            
-            categories: [
-                "electronics","groceries","mobiles"
-            ]
-        }
+      super();
+       this.state = {
+         sections: [
+           {
+             title: "veggies",
+             imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+             id: 1,
+             linkUrl: "veggies",
+           },
+           {
+             title: "jackets",
+             imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+             id: 2,
+             linkUrl: "",
+           },
+           {
+             title: "sneakers",
+             imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+             id: 3,
+             linkUrl: "",
+           },
+           {
+             title: "womens",
+             imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+             size: "large",
+             id: 4,
+             linkUrl: "",
+           },
+           {
+             title: "mens",
+             imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+             size: "large",
+             id: 5,
+             linkUrl: "",
+           },
+         ]
+       };
     }
 
 
@@ -21,7 +51,9 @@ class Directory extends React.Component {
     return (
       <div className="directory-component">
           {
-              this.state.categories.map((category)=> <p>{category}</p>)
+              this.state.sections.map(({id, ...otherSectionProps}) => {
+                  console.log(otherSectionProps);
+              })
           }
       </div>
     );
@@ -29,3 +61,8 @@ class Directory extends React.Component {
 }
 
 export default Directory;
+
+
+
+
+
