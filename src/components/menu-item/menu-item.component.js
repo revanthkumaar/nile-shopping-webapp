e1cd3855891df9d4778f753bc25dfdb831fb7ea8
revanthkumaar <iamrevanthkumar@gmail.com>
2021-08-14 19:29:29 +0530

    withrouter added

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,20 +1,30 @@
 import React from "react";
-
+import {withRouter} from "react-router-dom"
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
-);
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match, location }) => { 
+  console.log(history);
+  console.log(match);
+  console.log(location);
+return(
 
-export default MenuItem;
+  <div className={`${size} menu-item`}>
+  <div
+    className="background-image"
+    style={{
+      backgroundImage: `url(${imageUrl})`,
+    }}
+  />
+  <div className="content">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <span className="subtitle">SHOP NOW</span>
+  </div>
+</div>
+
+)
+
+
+}
+  
+
+export default withRouter(MenuItem);
